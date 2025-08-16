965b39d12dbc38c05066a71c401f3f5b483d51fc
Yuurij-dev <yurijesus.dev@gmail.com>
2025-08-15 22:45:14 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <Button>CLime me</Button>
     </div>
-  );
+  )
 }
