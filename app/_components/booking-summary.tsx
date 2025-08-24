c231d6276463e273bb17586866621afd219371c7
Yuurij-dev <yurijesus.dev@gmail.com>
2025-08-23 21:58:01 -0300

    feat: ensure booking details can be viewed

---
+++ app/_components/booking-summary.tsx
@@ -0,0 +1,40 @@
+// // import { Card, CardContent } from "./ui/card"
+
+// const BookingSummary = () => {
+//   return (
+//     // <Card>
+//     //   <CardContent className="space-y-3 p-3">
+//     //     <div className="flex items-center justify-between">
+//     //       <h2 className="font-bold">{service.name}</h2>
+//     //       <p className="text-sm font-bold">
+//     //         {Intl.NumberFormat("pt-BR", {
+//     //           style: "currency",
+//     //           currency: "BRL",
+//     //         }).format(Number(service.price))}
+//     //       </p>
+//     //     </div>
+
+//     //     <div className="flex items-center justify-between">
+//     //       <h2 className="text-sm text-gray-400">Data</h2>
+//     //       <p className="text-sm">
+//     //         {format(selectedDay, "d 'de' MMMM", {
+//     //           locale: ptBR,
+//     //         })}
+//     //       </p>
+//     //     </div>
+
+//     //     <div className="flex items-center justify-between">
+//     //       <h2 className="text-sm text-gray-400">Horário</h2>
+//     //       <p className="text-sm">{selectedTime}</p>
+//     //     </div>
+
+//     //     <div className="flex items-center justify-between">
+//     //       <h2 className="text-sm text-gray-400">Barbearia</h2>
+//     //       <p className="text-sm">{barberShop.name}</p>
+//     //     </div>
+//     //   </CardContent>
+//     // </Card>
+//   // )
+// }
+
+// export default BookingSummary
