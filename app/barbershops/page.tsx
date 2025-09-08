4bae09d357bca76015a62619d11fa3c28002e7cd
Yuurij-dev <yurijesus.dev@gmail.com>
2025-09-08 01:50:41 -0300

    feat(responsive): improve layout and component styles desktop e tablet on barbershop search

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,5 +1,6 @@
 import BarberShopItem from "../_components/barbershop-item"
 import Header from "../_components/header"
+import HeaderDesktop from "../_components/header-desktop"
 import Search from "../_components/search"
 import { getBarberShopsByTitleOrService } from "../_data/get-barbershops-by-title-or-service"
 
@@ -15,16 +16,18 @@ const BarberShopsPage = async ({ searchParams }: BarberShopProps) => {
   return (
     <div>
       <Header />
-      <div className="my-6 px-5">
+      <HeaderDesktop />
+      <div className="my-6 px-5 md:hidden">
         <Search />
       </div>
 
-      <div className="mb-6 px-5">
-        <h2 className="upercase mb-3 mt-6 text-xs font-bold text-gray-400">
-          Resultados para {searchParams.title || searchParams.service}
+      <div className="mb-6 px-5 md:m-auto md:max-w-[1500px]">
+        <h2 className="upercase mb-3 mt-6 text-xs font-bold text-gray-400 md:text-lg md:text-white">
+          Resultados para &quot;{searchParams.title || searchParams.service}
+          &quot;
         </h2>
 
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
           {barberShops.map((barberShop) => (
             <BarberShopItem key={barberShop.id} barbershop={barberShop} />
           ))}
